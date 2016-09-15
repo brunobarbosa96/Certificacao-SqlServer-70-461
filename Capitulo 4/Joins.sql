d0b72df33db173bf018bcd8b6231eaaa604cbb89
Bruno Barbosa <brunobarbosa96@hotmail.com>
2016-09-15 20:45:51 -0300

    Exemplos Joins

--- Capitulo 4/Joins.sql
+++ Capitulo 4/Joins.sql
@@ -27,3 +27,86 @@ SELECT S.companyname AS supplier,
 		INNER JOIN [Production].[Products] AS P
 			ON S.supplierid = P.supplierid
 		WHERE S.country LIKE N'Japan';
+
+
+-- Exemplo LEFT OUTER JOIN ou LEFT JOIN da forma simplificada
+-- o left join tras todos os registros da tabela da esquerda independentemente se tem ou não na tabela da direita,
+-- quando não encontra registro na segunda tabela o resultado retornado vem null
+-- Exemplo
+
+SELECT
+	S.companyname AS supplier, S.country,
+	P.productid, P.productname, P.unitprice
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN Production.Products AS P
+			ON S.supplierid = P.supplierid
+	WHERE S.country = N'Japan';
+
+
+-- Multi join - é possível combinar múltiplos joins em uma mesma query, por exemplo:
+
+SELECT
+	S.companyname AS supplier, S.country,
+	P.productid, P.productname, P.unitprice,
+	C.categoryname
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN Production.Products AS P
+			ON S.supplierid = P.supplierid
+		INNER JOIN Production.Categories AS C
+			ON C.categoryid = P.categoryid
+	WHERE S.country = N'Japan';
+
+
+
+	SELECT
+		S.companyname AS supplier, S.country,
+		P.productid, P.productname, P.unitprice,
+		C.categoryname
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN
+			(Production.Products AS P
+				INNER JOIN Production.Categories AS C
+				ON C.categoryid = P.categoryid)
+		ON S.supplierid = P.supplierid
+	WHERE S.country = N'Japan';
+
+
+-- Exercise
+-- Join
+SELECT C.CustId,
+	   C.CompanyName,
+	   O.OrderId,
+	   O.Orderdate
+	FROM [Sales].[Customers] C WITH(NOLOCK)
+		INNER JOIN [Sales].[Orders] O WITH(NOLOCK)
+			ON O.CustId = C.CustId
+
+-- Outer Join
+SELECT C.CustId,
+	   C.CompanyName,
+	   O.OrderId,
+	   O.Orderdate
+	FROM [Sales].[Customers] C WITH(NOLOCK)
+		LEFT OUTER JOIN [Sales].[Orders] O WITH(NOLOCK)
+			ON O.CustId = C.CustId
+
+-- apenas os customers sem orders
+SELECT C.CustId,
+	   C.CompanyName,
+	   O.OrderId,
+	   O.Orderdate
+	FROM [Sales].[Customers] C WITH(NOLOCK)
+		LEFT OUTER JOIN [Sales].[Orders] O WITH(NOLOCK)
+			ON O.CustId = C.CustId
+	WHERE O.CustId IS NULL
+
+-- Adicionando filtros
+SELECT C.CustId,
+	   C.CompanyName,
+	   O.OrderId,
+	   O.Orderdate
+	FROM [Sales].[Customers] C WITH(NOLOCK)
+		LEFT OUTER JOIN [Sales].[Orders] O WITH(NOLOCK)
+			ON O.CustId = C.CustId
+				AND O.OrderDate >= '20080201'
+				AND O.OrderDate < '20080301'
